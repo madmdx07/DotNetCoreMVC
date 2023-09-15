d6b37da3035cbda2a3c361a4109ffff9478e0061
madmdx07 <mahessdhant@gmail.com>
2023-09-15 12:42:42 +0545

    Add project files.

---
+++ FilesInCore/Controllers/SingleFileController.cs
@@ -0,0 +1,43 @@
+﻿using FilesInCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilesInCore.Controllers
+{
+    public class SingleFileController : Controller
+    {
+        public IActionResult Index()
+        {
+            SingleFileModel model = new SingleFileModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Upload(SingleFileModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.IsResponse = true;
+
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files");
+
+                //create folder if not exist
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                //get file extension
+                FileInfo fileInfo = new FileInfo(model.File.FileName);
+                string fileName = model.FileName + fileInfo.Extension;
+
+                string fileNameWithPath = Path.Combine(path, fileName);
+
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    model.File.CopyTo(stream);
+                }
+                model.IsSuccess = true;
+                model.Message = "File upload successfully";
+            }
+            return View("Index", model);
+        }
+    }
+}
---
+++ FilesInCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FilesInCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
